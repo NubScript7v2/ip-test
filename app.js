9d7f1410c467f7b49e445f2eeb810177b9bf83c4
NubScript7 <112324399+NubScript7@users.noreply.github.com>
2023-05-12 22:35:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require("express");
 
+const reqIP = require('request-ip');
+
+const app = express();
+
+app.get("/",(req,res)=>{
+
+ let clip = reqIP.getClientIp(req);
+
+ res.send("the ip address is "+clip)
+
+})
+
+app.listen(process.env.PORT || 3000)
