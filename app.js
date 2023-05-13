e835cdcca3bfebfddcc50ad3a67eaa9472a31ee6
NubScript7v2 <133381181+NubScript7v2@users.noreply.github.com>
2023-05-13 08:56:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+const express = require('express');
+
+const proxyaddr = require('proxy-addr');
+
+const app = express();
+
+// Customize the trusted proxy settings
+
+app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']);
+
+// Middleware to get client IP address
+
+app.use((req, res, next) => {
+
+  const ipAddress = proxyaddr(req, ['loopback', 'linklocal', 'uniquelocal']);
+
+  req.clientIP = ipAddress;
+
+  next();
+
+});
+
+// Example route that uses the client IP address
+
+app.get('/ip', (req, res) => {
+
+  res.send(`Your IP address is: ${req.clientIP}`);
+
+});
+
+// Start the server
+
+app.listen(3000, () => {
+
+  console.log('Server started on port 3000');
+
+});
+
+/*
 const express = require("express");
 
 const reqIP = require('request-ip');
@@ -13,3 +52,4 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(process.env.PORT || 3000)
+*/
